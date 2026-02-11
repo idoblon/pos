c8c108a9bdc26e913eaf0d290a982fcfe8db1ebe
idoblon <enqriquednes@gmail.com>
2026-02-11 18:30:42 +0545

    POS

---
+++ POS/src/main/java/com/springboot/POS/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.springboot.POS.configuration;
+
+public class JwtConstant {
+    public static final String JWT_SECRET = "jfajigskasej,kajeriegj,ieurifgakgdg,skqritgjas";
+    public static final String JWT_HEADER = "Authorization";
+}
